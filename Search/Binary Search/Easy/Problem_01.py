9d135ff89712fdd58fb6ce6c502012de8b7d7888
JadoGarBhai <sikandernawaz35@gmail.com>
2024-06-29 23:09:11 +0500

    01

---
+++ Search/Binary Search/Easy/Problem_01.py
@@ -0,0 +1,32 @@
+# Given a non-negative integer x, return the square root of x rounded down to the nearest integer. The returned integer should be non-negative as well.
+# You must not use any built-in exponent function or operator.
+# For example, do not use pow(x, 0.5) in c++ or x ** 0.5 in python.
+
+# Example 1:
+# Input: x = 4
+# Output: 2
+# Explanation: The square root of 4 is 2, so we return 2.
+
+# SOLUTION:
+
+
+def mySqrt(self, x):
+     if x < 2:
+          return x
+
+     left, right = 0, x
+
+     while left <= right:
+          mid = (left + right) // 2
+          mid_squared = mid * mid
+
+          if mid_squared == x:
+               return mid
+          elif mid_squared < x:
+               left = mid + 1
+          else:
+               right = mid - 1
+
+     return right
+
+mySqrt(4)
---
+++ Search/Binary Search/README.md
@@ -0,0 +1,5 @@
+# <div align='center'>Problem Links</div>
+
+- ## Easy
+
+1. [69. Sqrt(x)](https://leetcode.com/problems/sqrtx)
